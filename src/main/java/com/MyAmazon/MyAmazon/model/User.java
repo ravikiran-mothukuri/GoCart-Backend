dbefc0e0c96f9d09aee9326e4e304696c5ac63d6
ravikiran-mothukuri <ravikiranmothukuri699@gmail.com>
2025-12-12 00:31:22 +0530

    fixed table name

--- src/main/java/com/MyAmazon/MyAmazon/model/User.java
+++ src/main/java/com/MyAmazon/MyAmazon/model/User.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
